48c4f8b31a4ab51dfc28e94c86a1ac23a1ea09f1
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-14 06:26:14 +0100

    Add form controller

---
+++ app/javascript/controllers/form_controller.js
@@ -0,0 +1,87 @@
+import { Controller } from 'stimulus'
+
+export default class extends Controller {
+  connect () {
+    this.element.setAttribute('novalidate', true)
+    this.element.addEventListener('blur', this.onBlur, true)
+    this.element.addEventListener('submit', this.onSubmit)
+    this.element.addEventListener('ajax:beforeSend', this.onSubmit)
+  }
+
+  disconnect () {
+    this.element.removeEventListener('blur', this.onBlur)
+    this.element.removeEventListener('submit', this.onSubmit)
+    this.element.removeEventListener('ajax:beforeSend', this.onSubmit)
+  }
+
+  onBlur = (event) => {
+    this.validateField(event.target)
+  }
+
+  onSubmit = (event) => {
+    if (!this.validateForm()) {
+      event.preventDefault()
+      this.firstInvalidField.focus()
+    }
+  }
+
+  submit () {
+    if (this.element.getAttribute('data-remote')) {
+      this.element.dispatchEvent(new CustomEvent('submit', { bubbles: true, cancelable: true }))
+    } else {
+      this.element.submit()
+    }
+  }
+
+  validateForm () {
+    let isValid = true
+    // Not using `find` because we want to validate all the fields
+    this.formFields.forEach((field) => {
+      if (this.shouldValidateField(field) && !this.validateField(field)) isValid = false
+    })
+    return isValid
+  }
+
+  validateField (field) {
+    if (!this.shouldValidateField(field))
+      return true
+    const isValid = field.checkValidity()
+    field.classList.toggle('invalid', !isValid)
+    this.refreshErrorForInvalidField(field, isValid)
+    return isValid
+  }
+
+  shouldValidateField (field) {
+    return !field.disabled && !['file', 'reset', 'submit', 'button'].includes(field.type)
+  }
+
+  refreshErrorForInvalidField (field, isValid) {
+    this.removeExistingErrorMessage(field)
+    if (!isValid)
+      this.showErrorForInvalidField(field)
+  }
+
+  removeExistingErrorMessage (field) {
+    const fieldContainer = field.closest('.field')
+    const existingErrorMessageElement = fieldContainer.querySelector('.error')
+    if (existingErrorMessageElement)
+      existingErrorMessageElement.parentNode.removeChild(existingErrorMessageElement)
+  }
+
+  showErrorForInvalidField (field) {
+    field.insertAdjacentHTML('afterend', this.buildFieldErrorHtml(field))
+  }
+
+  buildFieldErrorHtml (field) {
+    return `<p class="error">${field.validationMessage}</p>`
+  }
+
+  get formFields () {
+    return Array.from(this.element.elements)
+  }
+
+  get firstInvalidField () {
+    return this.formFields.find(field => !field.checkValidity())
+  }
+
+}
