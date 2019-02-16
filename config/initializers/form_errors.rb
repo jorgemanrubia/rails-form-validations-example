ccd7cca111196eaae21c6ef68af5f64495927711
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-16 19:11:29 +0100

    Extract variable to remove duplicate `html_safe` call

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -7,14 +7,16 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance_tag|
   object = instance_tag.object
   error_message = object.errors.full_messages.join(', ')
 
-  if field
-    field['class'] = "#{field['class']} invalid"
-    html = <<-HTML
+  html = if field
+           field['class'] = "#{field['class']} invalid"
+           html = <<-HTML
 			#{fragment.to_s}
 			<p class="error">#{error_message}</p>
-    HTML
-    html.html_safe
-  else
-    html_tag.html_safe
-  end
+           HTML
+           html
+         else
+           html_tag
+         end
+
+  html.html_safe
 end
