3b2751beeed787bef753afef3d7f37e21b6bd5d6
Jorge Manrubia <jorge.manrubia@gmail.com>
2019-02-16 16:41:58 +0100

    Add rails_ujs via webpacker instead of asset pipeline

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,3 +13,5 @@ import { definitionsFromContext } from "stimulus/webpack-helpers"
 const application = Application.start()
 const context = require.context("../controllers", true, /\.js$/)
 application.load(definitionsFromContext(context))
+
+require("@rails/ujs").start()
